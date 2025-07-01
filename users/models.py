c9787043645d0c2030fb803ab8dbfaf46f1c6c9d
oofrog <78217274+oofrog@users.noreply.github.com>
2025-07-01 22:56:37 +0900

    Create User model

---
+++ users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    avatar = models.ImageField(
+        blank=True,
+    )
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
+    description = models.TextField()
