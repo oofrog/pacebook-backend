906ed0a0f5dc799041785587f2325cc4ac538f04
oofrog <78217274+oofrog@users.noreply.github.com>
2025-07-19 17:00:36 +0900

    Create PUT PostDetail

--- community/views.py
+++ community/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Post
 from .serializers import PostListSerializer, PostDetailSerializer
+from rest_framework.status import HTTP_204_NO_CONTENT
 
 
 class Posts(APIView):
@@ -32,6 +33,8 @@ class Posts(APIView):
 
 class PostDetail(APIView):
 
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get_object(self, pk):
         try:
             return Post.objects.get(pk=pk)
@@ -42,3 +45,27 @@ class PostDetail(APIView):
         post = self.get_object(pk)
         serializer = PostDetailSerializer(post)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        if post.owner != request.user:
+            raise PermissionDenied
+        serializer = PostDetailSerializer(
+            post,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_post = serializer.save()
+            serializer = PostDetailSerializer(updated_post)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        if post.owner != request.user:
+            raise PermissionDenied
+        post.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
