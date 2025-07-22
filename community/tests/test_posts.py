9890b36d0963e1e016a677403a2b801756afc685
oofrog <78217274+oofrog@users.noreply.github.com>
2025-07-22 15:55:54 +0900

    test(posts): POST 테스트 추가

--- community/tests/test_posts.py
+++ community/tests/test_posts.py
@@ -6,6 +6,7 @@ from community.models import Post
 class TestPosts(APITestCase):
 
     TEXT = "test desc"
+    URL = "/api/v1/posts/"
 
     def setUp(self):
         user = User.objects.create(
@@ -22,9 +23,9 @@ class TestPosts(APITestCase):
 
     def test_get_posts(self):
 
-        response = self.client.get("/api/v1/posts/")
+        response = self.client.get(self.URL)
         self.assertEqual(response.status_code, 200, "not 200")
-        data = response.data
+        data = response.json()
         self.assertIsInstance(
             data,
             list,
@@ -39,5 +40,55 @@ class TestPosts(APITestCase):
         )
         self.assertEqual(
             data[0]["payload"],
+            self.TEXT,
+        )
+
+    def test_create_post(self):
+
+        # unauth
+        response = self.client.post(self.URL)
+        self.assertEqual(
+            response.status_code,
+            403,
+        )
+
+        # auth
+        self.client.force_login(self.user)
+        # no content
+        response = self.client.post(self.URL)
+        self.assertEqual(
+            response.status_code,
+            400,
+        )
+        # content
+        response = self.client.post(
+            self.URL,
+            data={
+                "payload": self.TEXT,
+            },
+        )
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+        data = response.json()
+        self.assertIn(
+            "owner",
+            data,
+        )
+        self.assertEqual(
+            data["owner"]["pk"],
+            self.user.pk,
+        )
+        self.assertEqual(
+            data["owner"]["username"],
+            self.user.username,
+        )
+        self.assertIn(
+            "payload",
+            data,
+        )
+        self.assertEqual(
+            data["payload"],
             self.TEXT,
         )
